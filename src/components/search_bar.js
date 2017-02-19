c2d54ccd9a809e6dacb41239799d71217b909719
Henry McC <henrymcc@gmail.com>
2017-02-19 16:25:49 +0200

    Made some nice changes to iterate through the 5 default videos displayed.  Added in nice style.css elements to make the design a little better with hover and cursors.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,13 +5,24 @@ import React,{Component} from 'react';
 // };
 
 class SearchBar extends Component {
-  render() {
-      return <input onChange={this.onInputChange} />;
+  //call constructor of the class and call Component's super
+  constructor(props){
+    super(props);
+
+    this.state = {term: ''}; // term is set to an empty string
   }
 
-  onInputChange(event){
-    console.log(event.target.value);
+  render() {
+      return(
+        <div className="search-bar">
+          <input
+            value={this.state.term}  //when state changes the value is changed with state's value
+            onChange={(event) => this.setState({term: event.target.value})} //when user enters text, the value here then updates term with the new value which then re-rendesr the whole "render()" and then sets the value with the terms' new value
+          />
+        </div>
+    );
   }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBqkuRI4j0Uf7P0Lnv6PyWwWgU7VYnoEz0';
 
-const App = () => {
-  return <div>
+class App extends Component {
+  constructor(props){
+    super(props);
 
-    <SearchBar />
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-  </div>;
+    //remembering this happens asynchronously, so its populated once completed.
+    YTSearch({key: API_KEY, term: 'drones'}, (videos) => {
+//this is setting the state above this.state section so whatever comes back gets populated above.
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //this.setState ({videos:videos});
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <SearchBar />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+
+    </div>
+  );
+  }
 }
+//video is a variable that gets sent as a parameter to the child class in this case VideoDetail
+//videos is a variable like the same as above except this child is VideoList
+//<VideoList videos={this.state.videos} />
+//the params that is passed above is called props
 
 ReactDOM.render(<App />, document.querySelector('.container'));
